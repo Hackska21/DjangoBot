8669da2fae21925d9a753871012df6437f3ec3b6
Jorge Romero <jorge.romero@parrotsoftware.io>
2025-05-17 06:57:12 -0600

    Use postgresqql instead sqlite

--- djangoBot/settings.py
+++ djangoBot/settings.py
@@ -86,10 +86,11 @@ WSGI_APPLICATION = 'djangoBot.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
